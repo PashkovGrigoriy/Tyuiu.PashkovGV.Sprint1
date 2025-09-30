c4a8e91da85065915b305241f31b622d63f7c75b
PashkovGrigoriy <gripas2006@gmail.com>
2025-09-30 23:57:32 +0500

    Task7 Lib

--- Tyuiu.PashkovGV.Sprint1.Task7.V14.Lib/DataService.cs
+++ Tyuiu.PashkovGV.Sprint1.Task7.V14.Lib/DataService.cs
@@ -1,7 +1,7 @@
 ﻿using tyuiu.cources.programming.interfaces.Sprint1;
 namespace Tyuiu.PashkovGV.Sprint1.Task7.V14.Lib
 {
-    public class DataService : ISprint1Task7V4
+    public class DataService : ISprint1Task7V14 
     {
         public double Calculate(double x, double y)
         {
